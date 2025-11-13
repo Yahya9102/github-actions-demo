7369a7834a3e78c42073ffcac6ddfe53255bb488
Yahya9102 <yaya_hussein@hotmail.com>
2025-11-13 10:45:13 +0100

    make test pass

--- src/calculator/calculator.test.ts
+++ src/calculator/calculator.test.ts
@@ -4,7 +4,9 @@ import {describe, it, expect} from "vitest";
 
 describe("testing add function", () => {
     it("Should return the sum of two numbers added", () => {
-        expect(add(2,3)).toBe(4)
+        expect(add(2,3)).toBe(5)
     })
+
+    // test passing now
 })
 
