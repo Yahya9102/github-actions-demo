fa77afef78d618de3f266437f05f3f7a78f342d9
Yahya9102 <yaya_hussein@hotmail.com>
2025-11-13 10:42:52 +0100

    Add testing framework and implement basic addition function with tests
    - Updated CI configuration to include test execution
    - Added vitest as a development dependency
    - Created calculator module with an add function
    - Implemented unit tests for the add function

---
+++ src/calculator/calculator.test.ts
@@ -0,0 +1,10 @@
+import { add } from "./calculator";
+import {describe, it, expect} from "vitest";
+
+
+describe("testing add function", () => {
+    it("Should return the sum of two numbers added", () => {
+        expect(add(2,3)).toBe(4)
+    })
+})
+
